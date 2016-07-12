02c1ef16811c426d5968e0f07083e745b39ad8d1
Anna Clawson <seadeer@gmail.com>
2016-07-12 13:57:57 -0700

    basic server runs

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var app = express();
 
 app.use(express.static(__dirname + '/client/assets'));
 app.use(bodyParser.json());
-require('./server/config/mongoose.js');
 var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
-
+app.set('views', path.join(__dirname, './client/assets/html'));
+app.set('view engine', 'ejs');
 app.listen(9000, function(){
     console.log("Listening on 9000");
 });
