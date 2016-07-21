1843696d5e98d7ccd0394262034ab33c01e3aec7
Andrew Mestas (InConsulting Inc.) <v-anmest@microsoft.com>
2016-07-21 15:04:20 -0700

    fix for .files

--- fileSystem.js
+++ fileSystem.js
@@ -6,12 +6,14 @@ var results = fs.readdirSync(folder);
 
     var data = [];
     for (var i in results){
-         var name = results[i].toString();
-         var folder = path.join(rootpath, '/client/assets/files/'+name);
-         var topics = fs.readdirSync(folder);
-         var obj = {};
-        obj[name] = topics;           
-        data.push(obj);
+        if(results[i][0] != '.'){        
+            var name = results[i].toString();
+            var folder = path.join(rootpath, '/client/assets/files/'+name);
+            var topics = fs.readdirSync(folder);
+            var obj = {};
+            obj[name] = topics;           
+            data.push(obj);
+        }
     }
 
     //console.log(data)
